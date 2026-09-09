0a07b653e44519d8dd0efc7187f1347bb0026312
kajal021 <kajal@eazotel.com>
2026-09-09 15:54:29 +0530

    ga4 added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,42 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  trailingSlash: true,
+
+  compress: true,
+
+  poweredByHeader: false,
+
+  reactStrictMode: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.facebook.com",
+      },
+    ],
+    formats: ["image/avif", "image/webp"],
+
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+
+    minimumCacheTTL: 2678400,
+
+    dangerouslyAllowSVG: true,
+
+    contentDispositionType: "attachment",
+  },
+
+  experimental: {
+    optimizePackageImports: ["react-datepicker", "axios", "swiper/react"],
+
+    optimizeCss: true,
+    optimisticClientCache: true,
+
+    scrollRestoration: true,
+  },
 };
 
 export default nextConfig;
--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,23 +1,22 @@
 import LinkButton from "@/src/components/buttons/LinkButton";
 import { SectionWithContainer } from "@/src/components/sectionComponants";
 
-
 export default function ThankYou() {
   return (
     <main>
-      <SectionWithContainer sectionClassName="bg-cream-bg">
+      <SectionWithContainer sectionClassName="bg-primary">
         <div className="flex flex-col gap-6 items-center justify-center">
           <div className="max-w-sm w-full relative py-14 px-16 items-center">
             {/* <FooterIcon/> */}
           </div>
-          <p className="text-p2">THANK YOU FOR SUBMITTING</p>
-          <h1 className="font-bold font-primary text-2xl md:text-5xl text-p2">
+          <p className="text-white">THANK YOU FOR SUBMITTING</p>
+          <h1 className="font-bold font-primary text-white text-2xl md:text-5xl text-p2">
             We will get back to you shortly!
           </h1>
           <LinkButton
             href="/"
             label="Back to Home"
-            className="w-fit mx-auto rounded-full"
+            className="w-fit mx-auto rounded-full text-white"
           />
         </div>
       </SectionWithContainer>
