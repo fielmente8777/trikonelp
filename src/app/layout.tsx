abaa497cd2c075b88fd52151ad2fe5badefc6dc6
kajal021 <kajal@eazotel.com>
2026-09-10 16:49:44 +0530

    tag added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,6 +64,14 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           name="google-site-verification"
           content="0qrxNkwwCN-jdfL-gzpf3NlWYKXuhXSjQBz3k-rYrYA"
         />
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+        >{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TML5XPBV');`}</Script>
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-3EGNKWTFKQ"
@@ -80,6 +88,14 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
         className={`${dmSans.variable} ${fraunces.variable} ${varelaRound.variable} ${cormorant.variable} h-full antialiased`}
         suppressHydrationWarning={true}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TML5XPBV"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <WebProvider>
           <OfferSection />
           <LandingNavbar />
