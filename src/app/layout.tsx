a4af7f897c5a9ae8bcf9214badf29d87a3100ad8
kajal021 <kajal@eazotel.com>
2026-09-22 17:29:34 +0530

    ga4 added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,11 +71,19 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K4C7HLSZ');`}</Script>
+        {/* <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+        >{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-TML5XPBV');`}</Script>
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-3EGNKWTFKQ"
-        ></script>
+        ></script> */}
         <Script id="google analytics" strategy="afterInteractive">
           {`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
@@ -90,12 +98,20 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       >
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TML5XPBV"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K4C7HLSZ"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
+        {/* <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TML5XPBV"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript> */}
         <WebProvider>
           <OfferSection />
           <LandingNavbar />
